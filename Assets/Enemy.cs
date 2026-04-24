190a990672548c231643ab1a96f3048ae6e495af
404-DevNotFound <dante.engineer@gmail.com>
2026-04-24 18:26:53 +0700

    enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+   public NavMeshAgent agent;
+
+   public Transform player;
+
+   public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
+
+    public Vector3 walkPoint;
+
+    bool walkPointSet;
+
+    public float walkPointRange;
+
+    //states
+    public float sightRange;
+    public bool playerInSightRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("First Person Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //Check for sight of player
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (!playerInSightRange) Patroling();
+        if (playerInSightRange) chaseplayer();
+    }
+    private void Patroling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+    private void chaseplayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    }
